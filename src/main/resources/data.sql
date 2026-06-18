2163d5b687638c3af16efafec36d67717e29267c
Diana Rodriguez <carolayd.r.b92@hotmail.com>
2026-06-18 06:26:05 -0500

    configuracion de registro en la base de datos e implementacion de endpoints

---
+++ src/main/resources/data.sql
@@ -0,0 +1,11 @@
+INSERT INTO currencies(code,name,exchange_rate,active)
+VALUES ('USD','US Dollar',1.00,true);
+
+INSERT INTO currencies(code,name,exchange_rate,active)
+VALUES ('COP','Peso Colombiano',4200,true);
+
+INSERT INTO currencies(code,name,exchange_rate,active)
+VALUES ('EUR','Euro',0.85,true);
+
+INSERT INTO currencies(code,name,exchange_rate,active)
+VALUES ('BRL','Real Brasileño',5.40,true);
